8fae7b478e75033f742617e846331f7759b3e3df
bkaiserinfosec <49665796+bkaiserinfosec@users.noreply.github.com>
2023-07-19 20:59:55 -0700

    Feature/fix jenkinsfile (#135)
    * move source code db table init to initial setup
    
    * add popup modal for loading during initial registration
    
    * add backticks for reserved mysql words
    
    * add update to is_admin
    
    * add db_connection_handler to handle bad network connections to db
    
    * remove general function
    
    * add 1 second pause after unsuccessful auth attempt
    
    * remove unneeded endpoints
    
    * add condition to set az auth method
    
    * initial commit
    
    * fix production path error
    
    * add conditions for running in sqlite environments
    
    * updates for unit test results and correction
    
    * Update web_testing.py
    
    * move web unit tests
    
    * update for unit testing
    
    * remove relative path setting
    
    * remove unused classes
    
    * remove foreign key constraint for PrivateNetworks
    
    * changed dast target to localhost
    
    * update to using service name
    
    * set static name for container
    
    * add jenkins webhook
    
    * remove InetAddress
    
    * remove def syntax
    
    * remove def syntax
    
    * add script container
    
    * add dynamic IP discovery
    
    * use groovy function to get node ip
    
    * Update Jenkinsfile
    
    * get build agent ip
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * create new app profile during loc delivery
    
    * add jenkins webhook
    
    * add conditions for testing env
    
    * add jenkins webhook and testing on demand
    
    * bug fixes
    
    * add appenvironmentdata endpoints
    
    * add fixes from unit testing
    
    * Update findings.py
    
    * Update findings.py
    
    * Update Jenkinsfile
    
    * add global app metrics
    
    * add all applications metrics to static order
    
    ---------
    
    Co-authored-by: bkaiserinfosec <112435093+SecurityUniversal@users.noreply.github.com>

--- ci_cd/unit_tests/web_testing.py
+++ ci_cd/unit_tests/web_testing.py
@@ -223,6 +223,11 @@ class TestWebApp(unittest.TestCase):
         response = self._post_test_handler(route, data_dict)
         assert response.status_code == 200
 
+    def test_A5_all_application_metrics_get(self):
+        route = f"/all_application_metrics"
+        response = self._get_test_handler(route)
+        assert response.status_code == 200
+
     def test_login(self):
         response = self._login()
         self._logout()
@@ -1393,6 +1398,8 @@ class TestWebApp(unittest.TestCase):
         route = f"/all_application_metrics"
         response = self._get_test_handler(route)
         assert response.status_code == 200
+
+
 
 
 def _three_o_two_handler(headers, target):
