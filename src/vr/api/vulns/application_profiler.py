0e5675653087fd4e14fd949a6cf932dde7aaf8f1
bkaiserinfosec <49665796+bkaiserinfosec@users.noreply.github.com>
2023-07-23 15:21:26 -0700

    Feature/fix jenkinsfile (#185)
    * move source code db table init to initial setup
    
    * add popup modal for loading during initial registration
    
    * add backticks for reserved mysql words
    
    * add update to is_admin
    
    * add db_connection_handler to handle bad network connections to db
    
    * remove general function
    
    * add 1 second pause after unsuccessful auth attempt
    
    * remove unneeded endpoints
    
    * add condition to set az auth method
    
    * initial commit
    
    * fix production path error
    
    * add conditions for running in sqlite environments
    
    * updates for unit test results and correction
    
    * Update web_testing.py
    
    * move web unit tests
    
    * update for unit testing
    
    * remove relative path setting
    
    * remove unused classes
    
    * remove foreign key constraint for PrivateNetworks
    
    * changed dast target to localhost
    
    * update to using service name
    
    * set static name for container
    
    * add jenkins webhook
    
    * remove InetAddress
    
    * remove def syntax
    
    * remove def syntax
    
    * add script container
    
    * add dynamic IP discovery
    
    * use groovy function to get node ip
    
    * Update Jenkinsfile
    
    * get build agent ip
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * create new app profile during loc delivery
    
    * add jenkins webhook
    
    * add conditions for testing env
    
    * add jenkins webhook and testing on demand
    
    * bug fixes
    
    * add appenvironmentdata endpoints
    
    * add fixes from unit testing
    
    * Update findings.py
    
    * Update findings.py
    
    * Update Jenkinsfile
    
    * add global app metrics
    
    * add all applications metrics to static order
    
    * Update metrics.py
    
    * Update metrics.py
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * test
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update .gitignore
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * add application profiles
    
    * unit test fix
    
    * add section for app components
    
    * Update application_profiler.py
    
    ---------
    
    Co-authored-by: bkaiserinfosec <112435093+SecurityUniversal@users.noreply.github.com>

--- src/vr/api/vulns/application_profiler.py
+++ src/vr/api/vulns/application_profiler.py
@@ -2,16 +2,13 @@ import datetime
 from vr import db, app
 from flask import jsonify, request
 from vr.api import api
-from vr.vulns.model.pipelinejobs import PipelineJobs
-from vr.vulns.model.sgconfigsettingsperjob import SgConfigSettingsPerJob
-from vr.vulns.model.sgresultsperjob import SgResultsPerJob
-from vr.api.vulns.vulnerabilities import get_app_id
 from vr.admin.functions import db_connection_handler
 from vr.admin.oauth2 import require_oauth
 from authlib.integrations.flask_oauth2 import current_token
 from vr.admin.auth_functions import verify_api_key, get_token_auth_header
 from vr.functions.routing_functions import check_entity_permissions
 from vr.assets.model.applicationprofiles import ApplicationProfiles, ApplicationProfilesSchema
+from vr.assets.model.businessapplications import BusinessApplications
 
 
 ERROR_RESP = "Error: Invalid API Request"
@@ -33,37 +30,69 @@ def get_application_profile(app_id):
             response = jsonify(vulns), 200
     return response
 
-@api.route('/add_application_profile/<app_id>', methods=['POST'])
+@api.route('/add_application_profile/<repo_name>', methods=['POST'])
 @require_oauth('write:vulnerabilities')
-def add_application_profile(app_id):
+def add_application_profile(repo_name):
     form = request.get_json()
-    _add_application_profile(app_id, form)
+    _add_application_profile(repo_name, form)
     return jsonify({"result": "Report Delivered"}), 200
 
 
-def _add_application_profile(app_id, form):
-    profile = ApplicationProfiles(
-        AppID=app_id,
-        SecretScanReq=form['SecretScanReq'],
-        SecretScanData=form['SecretScanData'],
-        SCAReq=form['SCAReq'],
-        SCAData=form['SCAData'],
-        SASTReq=form['SASTReq'],
-        SASTData=form['SASTData'],
-        IACReq=form['IACReq'],
-        IACData=form['IACData'],
-        ContainerReq=form['ContainerReq'],
-        ContainerData=form['ContainerData'],
-        InfrastructureScanReq=form['InfrastructureScanReq'],
-        InfrastructureScanData=form['InfrastructureScanData'],
-        DASTReq=form['DASTReq'],
-        DASTData=form['DASTData'],
-        DASTApiReq=form['DASTApiReq'],
-        DASTApiData=form['DASTApiData'],
-        CICDConfigLocations=form['CICDConfigLocations'],
-        CICDConfigData=form['CICDConfigData']
-    )
-    db.session.add(profile)
-    db_connection_handler(db)
+def _add_application_profile(repo_name, form):
+    app_obj = BusinessApplications.query.filter(BusinessApplications.ApplicationAcronym.ilike(repo_name)).first()
+    if app_obj:
+        # Try to get the existing profile
+        profile = ApplicationProfiles.query.filter_by(AppID=app_obj.ID).first()
+
+        # If the profile exists, update its properties
+        if profile:
+            profile.AddDate = datetime.datetime.utcnow()
+            profile.SecretScanReq = form['SecretScanReq']
+            profile.SecretScanData = form['SecretScanData']
+            profile.SCAReq = form['SCAReq']
+            profile.SCAData = form['SCAData']
+            profile.SASTReq = form['SASTReq']
+            profile.SASTData = form['SASTData']
+            profile.IACReq = form['IACReq']
+            profile.IACData = form['IACData']
+            profile.ContainerReq = form['ContainerReq']
+            profile.ContainerData = form['ContainerData']
+            profile.InfrastructureScanReq = form['InfrastructureScanReq']
+            profile.InfrastructureScanData = form['InfrastructureScanData']
+            profile.DASTReq = form['DASTReq']
+            profile.DASTData = form['DASTData']
+            profile.DASTApiReq = form['DASTApiReq']
+            profile.DASTApiData = form['DASTApiData']
+            profile.CICDConfigLocations = form['CICDConfigLocations']
+            profile.CICDConfigData = form['CICDConfigData']
+
+        # If the profile does not exist, create a new one
+        else:
+            profile = ApplicationProfiles(
+                AppID=app_obj.ID,
+                SecretScanReq=form['SecretScanReq'],
+                SecretScanData=form['SecretScanData'],
+                SCAReq=form['SCAReq'],
+                SCAData=form['SCAData'],
+                SASTReq=form['SASTReq'],
+                SASTData=form['SASTData'],
+                IACReq=form['IACReq'],
+                IACData=form['IACData'],
+                ContainerReq=form['ContainerReq'],
+                ContainerData=form['ContainerData'],
+                InfrastructureScanReq=form['InfrastructureScanReq'],
+                InfrastructureScanData=form['InfrastructureScanData'],
+                DASTReq=form['DASTReq'],
+                DASTData=form['DASTData'],
+                DASTApiReq=form['DASTApiReq'],
+                DASTApiData=form['DASTApiData'],
+                CICDConfigLocations=form['CICDConfigLocations'],
+                CICDConfigData=form['CICDConfigData']
+            )
+            db.session.add(profile)
+
+        # Commit the changes to the database
+        db_connection_handler(db)
+
 
 
