b4f453c1bacd4ce7ae6a9760c47388a1ff2541f9
bkaiserinfosec <49665796+bkaiserinfosec@users.noreply.github.com>
2023-07-24 17:05:33 -0700

    Feature/fix jenkinsfile (#193)
    * move source code db table init to initial setup
    
    * add popup modal for loading during initial registration
    
    * add backticks for reserved mysql words
    
    * add update to is_admin
    
    * add db_connection_handler to handle bad network connections to db
    
    * remove general function
    
    * add 1 second pause after unsuccessful auth attempt
    
    * remove unneeded endpoints
    
    * add condition to set az auth method
    
    * initial commit
    
    * fix production path error
    
    * add conditions for running in sqlite environments
    
    * updates for unit test results and correction
    
    * Update web_testing.py
    
    * move web unit tests
    
    * update for unit testing
    
    * remove relative path setting
    
    * remove unused classes
    
    * remove foreign key constraint for PrivateNetworks
    
    * changed dast target to localhost
    
    * update to using service name
    
    * set static name for container
    
    * add jenkins webhook
    
    * remove InetAddress
    
    * remove def syntax
    
    * remove def syntax
    
    * add script container
    
    * add dynamic IP discovery
    
    * use groovy function to get node ip
    
    * Update Jenkinsfile
    
    * get build agent ip
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * create new app profile during loc delivery
    
    * add jenkins webhook
    
    * add conditions for testing env
    
    * add jenkins webhook and testing on demand
    
    * bug fixes
    
    * add appenvironmentdata endpoints
    
    * add fixes from unit testing
    
    * Update findings.py
    
    * Update findings.py
    
    * Update Jenkinsfile
    
    * add global app metrics
    
    * add all applications metrics to static order
    
    * Update metrics.py
    
    * Update metrics.py
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * test
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update .gitignore
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * add application profiles
    
    * unit test fix
    
    * add section for app components
    
    * Update application_profiler.py
    
    * add sample kpis
    
    * add application metrics to menu
    
    * Update Jenkinsfile
    
    * Update vulnerabilities.py
    
    ---------
    
    Co-authored-by: bkaiserinfosec <112435093+SecurityUniversal@users.noreply.github.com>

--- src/vr/api/vulns/vulnerabilities.py
+++ src/vr/api/vulns/vulnerabilities.py
@@ -213,9 +213,9 @@ def add_app_id_to_docker_img(docker_img_id, app_cmdb_id, app_id_list):
 
 def get_app_id(app_name, git_url):
     app_component = ''
-    if '---' in app_name:
-        app_name = app_name.split('---')[0]
-        app_component = app_name.split('---')[1]
+    if '--' in app_name:
+        app_name = app_name.split('--')[0]
+        app_component = app_name.split('--')[1]
     app = BusinessApplications.query.filter(text(f"BusinessApplications.ApplicationName='{app_name}' AND BusinessApplications.ApplicationAcronym='{app_component}'")).first()
     if app:
         app_id = app.ID
