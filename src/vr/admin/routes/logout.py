76e6f3ecf9e2ae7893cb23124997597017b858e9
bkaiserinfosec <112435093+SecurityUniversal@users.noreply.github.com>
2023-08-14 06:31:27 -0700

    add azure ad authentication and authoriztaion

--- src/vr/admin/routes/logout.py
+++ src/vr/admin/routes/logout.py
@@ -1,12 +1,23 @@
 from flask_login import logout_user, login_required
 from flask import session, redirect, url_for
 from vr.admin import admin
+from vr import app
+from config_engine import AUTH_TYPE
+
 
 NAV_CAT= { "name": "Admin", "url": "admin.admin_dashboard"}
 
-@admin.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    del session['username']
-    return redirect(url_for('admin.login'))
+
+if AUTH_TYPE == 'local':
+    @admin.route('/logout')
+    @login_required
+    def logout():
+        logout_user()
+        del session['username']
+        return redirect(url_for('admin.login'))
+elif AUTH_TYPE == 'azuread':
+    @admin.route('/logout')
+    def logout():
+        logout_user()
+        session.clear()  # Wipe out user and its token cache from session
+        return redirect(url_for('admin.login'))
