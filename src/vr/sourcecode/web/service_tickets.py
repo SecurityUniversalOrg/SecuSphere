f5e6686c6ca1ddedf83ea3da6b761e6751b80f7e
bkaiserinfosec <49665796+bkaiserinfosec@users.noreply.github.com>
2025-06-30 10:18:30 -0700

    Revert "Potential fix for code scanning alert no. 176: SQL query built from u…"
    This reverts commit 32d7ba361389e372af5de74f82f4a305479e08b8.

--- src/vr/sourcecode/web/service_tickets.py
+++ src/vr/sourcecode/web/service_tickets.py
@@ -111,7 +111,7 @@ def issue(appid, id):
             .first()
         NAV['appbar'] = 'workflows'
         app = BusinessApplications.query\
-            .filter(text('ID = :appid')).params(appid=appid).first()
+            .filter(text(f'ID={appid}')).first()
         app_data = {'ID': appid, 'ApplicationName': app.ApplicationName}
         return render_template('sourcecode/view_service_ticket.html', details=assets_all, app_data=app_data, user=user, NAV=NAV)
     except RuntimeError:
